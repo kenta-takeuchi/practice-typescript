d11290e2b57920e41a2530b7ddc8b3174a06ad42
Kenta Takeuchi <tkuc.kenta@gmail.com>
2020-11-22 22:04:51 +0900

    interface

--- interface.ts
+++ interface.ts
@@ -1,10 +1,20 @@
 interface Human {
     name: string;
     age: number;
+    greeting(message: string): void;
 }
 
 const human: Human = {
     name: 'test',
-    age: 30
+    age: 30,
+    greeting(message: string) {
+        console.log(message)
+    }
 }
 
+class Developer implements Human {
+    constructor(public name: string, public age: number) {}
+    greeting(message: string) {
+        console.log('test')
+    }
+}
