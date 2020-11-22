a6c66d6107c963e20b9b8ab1204e312a8a5746a3
Kenta Takeuchi <tkuc.kenta@gmail.com>
2020-11-23 00:47:47 +0900

    generics

--- dist/generics.js
+++ dist/generics.js
@@ -5,3 +5,23 @@ function copy(value, key) {
     return value;
 }
 console.log(copy({ name: 'test' }, 'name'));
+class LightDatabase {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LightDatabase();
+stringLightDatabase.add('apple');
+stringLightDatabase.add('banana');
+stringLightDatabase.add('grape');
+stringLightDatabase.remove('banana');
+console.log(stringLightDatabase.get());
