24fef145d207499132fc0323ad299b0ef1d49a4e
Kenta Takeuchi <tkuc.kenta@gmail.com>
2020-11-23 00:44:08 +0900

    generics

--- generics.ts
+++ generics.ts
@@ -1,5 +1,7 @@
-function copy<T>(value: T): T{
+function copy<T extends {name: string}, U extends keyof T>(value: T, key: U): T{
+    const name = value[key];
+    console.log(name);
     return value
 }
 
-console.log(copy({name: 'test'}).name)
+console.log(copy({name: 'test'}, 'name'))
