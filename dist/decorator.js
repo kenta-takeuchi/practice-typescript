0bc51bc4b04b71e4849c01a4ff4f0adb99f4d197
Kenta Takeuchi <tkuc.kenta@gmail.com>
2020-11-29 21:11:04 +0900

    food-app

---
+++ dist/decorator.js
@@ -0,0 +1,48 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logging(message) {
+    return function (constructor) {
+        console.log(message);
+        console.log(constructor);
+    };
+}
+function Component(template, selector) {
+    return function (constructor) {
+        return class extends constructor {
+            constructor(...args) {
+                super(...args);
+                const mountedElement = document.querySelector(selector);
+                const instance = new constructor();
+                if (mountedElement) {
+                    mountedElement.innerHTML = template;
+                    mountedElement.querySelector('h1').textContent = instance.name;
+                }
+            }
+        };
+    };
+}
+function PropertyLogging(target, propertyKey) {
+    console.log('propertyLogging');
+    console.log(target);
+    console.log(propertyKey);
+}
+let User = class User {
+    constructor(age) {
+        this.age = age;
+        this.name = 'Quilla';
+        console.log('User was created');
+    }
+};
+__decorate([
+    PropertyLogging
+], User.prototype, "name", void 0);
+User = __decorate([
+    Component('<h1>{{ name }}</h1>', '#app'),
+    Logging('Logging User')
+], User);
+const user1 = new User(32);
---
+++ typescript-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from "./components/Hello";
+
+const App = () => {
+  return (
+    <div>
+      <Hello message="I am" />
+    </div>
+  );
+}
+
+export default App;
